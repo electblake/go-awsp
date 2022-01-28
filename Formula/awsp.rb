dbde9b0d10adfa9711534d2ed08abda8c23f2818
electblake <electblake@gmail.com>
2022-01-27 20:56:32 -0500

    update formula to 0.1.2

--- Formula/awsp.rb
+++ Formula/awsp.rb
@@ -1,9 +1,9 @@
 class Awsp < Formula
     desc "Allows for easy switching between AWS profiles"
     homepage "https://github.com/electblake/homebrew-awsp"
-    url "https://github.com/electblake/homebrew-awsp/archive/refs/tags/v0.1.1.tar.gz"
-    version "0.1.1"
-    sha256 "69716915e08cda0e6bf23c968aa26bd4550d7b61608ad27604552907fdf810f7"
+    url "https://github.com/electblake/homebrew-awsp/archive/refs/tags/v0.1.2.tar.gz"
+    version "0.1.2"
+    sha256 "bb9879069efd71126249921dc13af0319c23b1f3471bb38574c506042a81a777"
   
     def install
       bin.install Dir["bin/_awsp"]
